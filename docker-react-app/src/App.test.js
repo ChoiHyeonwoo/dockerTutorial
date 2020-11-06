48b533d1a7518167724a206979f8c2483f7e972a
hyeonwoo <escape0619@gmail.com>
2020-11-06 21:30:59 +0900

    운영환경 nginx 베이스 이미지를 통한 빌드 및 실행

--- docker-react-app/src/App.test.js
+++ docker-react-app/src/App.test.js
@@ -3,6 +3,17 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/docker-compose로 했지롱! docker-compose 개편해요/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement1 = screen.getByText(/docker-compose로 했지롱! docker-compose 개편해요/i);
+  expect(linkElement1).toBeInTheDocument();
+});
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement2 = screen.getByText(/docker-compose로 했지롱! docker-compose 개편해요/i);
+  expect(linkElement2).toBeInTheDocument();
 });
